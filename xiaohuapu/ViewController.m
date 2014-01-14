c858c6696ea2bb02c4bea4b2ee8f9c3c642c05b6
lulu <lulu@lulutekiMacBook-Pro.local>
2014-01-14 14:52:11 +0800

    按时间排序

--- xiaohuapu/ViewController.m
+++ xiaohuapu/ViewController.m
@@ -52,10 +52,10 @@
     NSUserDefaults *userDefaultes = [NSUserDefaults standardUserDefaults];
     page = [userDefaultes integerForKey:@"page"] + 1;
     NSLog(@"page===%d",page);
-    order = 2;
+    order = 1;
 
     self.dataArray = [NSMutableArray array];
-    [self getData:page andOrder:2 andtime:minTime];
+    [self getData:page andOrder:1 andtime:minTime];
     
     hasMore = YES;
     isLoading = NO;
@@ -76,13 +76,13 @@
             return;
         }
         page++;
-        order = 2;
+        order = 1;
         [self getData:page andOrder:order andtime:maxTime];
     };
 }
 - (void)getData:(int)p andOrder:(int)o andtime:(NSString *)so0
 {
-    NSString *url = [NSString stringWithFormat:@"http://z.turbopush.com/jokelist.php?p=%d&o=%d&so=%@",page,o,so0];
+    NSString *url = [NSString stringWithFormat:@"http://z.turbopush.com/jokelist.php?p=%d&o=%d&so=%@",page,o,[so0 stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
     ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:url]];
     NSMutableDictionary *info = [NSMutableDictionary dictionary];
     [info setObject:[NSNumber numberWithInteger:p] forKey:@"page"];
@@ -101,13 +101,14 @@
     [self.mytableView reloadData];
     [self reloadDeals];
 }
+
 - (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView
 {
     page++;
-    order = 1;
+    order = 2;
+    temp = 2;
     [self getData:page andOrder:order andtime:minTime];
 }
-
 
 - (void)reloadDeals
 {
@@ -126,12 +127,18 @@
     NSDictionary  *info = [httprequest userInfo];
     NSNumber  *page1 = [info objectForKey:@"page"];
     NSDictionary * dic = [NSJSONSerialization JSONObjectWithData:httprequest.responseData options:NSJSONReadingMutableContainers error:nil];
-    NSLog(@"-=-=-=-=-=-=-=-=-%@",dic);
     if ([page1 intValue] == 0) {
         [self.dataArray removeAllObjects];
     }
-    
-    [self.dataArray addObjectsFromArray:[dic objectForKey:@"data"]];
+    if (temp == 2) {
+        for (int i = 0;i<[[dic objectForKey:@"data"] count]; i++) {
+            [self.dataArray insertObject:[[dic objectForKey:@"data"] objectAtIndex:i] atIndex:0];
+            }
+    }else{
+        [self.dataArray addObjectsFromArray:[dic objectForKey:@"data"]];
+    }
+//    [self.dataArray addObjectsFromArray:[dic objectForKey:@"data"]];
+//    NSLog(@"%@",self.dataArray);
 //    for (int i = 0;i<[[dic objectForKey:@"data"] count]; i++) {
 //        [self.dataArray insertObject:[[dic objectForKey:@"data"] objectAtIndex:i] atIndex:0];
 //    }
@@ -143,8 +150,6 @@
     
     minTime = [[self.dataArray objectAtIndex:0] objectForKey:@"date"];
     maxTime = [[self.dataArray objectAtIndex:[self.dataArray count]-1] objectForKey:@"date"];
-    NSLog(@"%@",[[self.dataArray objectAtIndex:0] objectForKey:@"date"]);
-    NSLog(@"%@",[[self.dataArray objectAtIndex:[self.dataArray count]-1] objectForKey:@"date"]);
 }
 
 - (void)requestFailed:(ASIHTTPRequest *)request
@@ -166,33 +171,6 @@
 {
     return [self.dataArray count];
 }
-
-//- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    static NSString *cellID = @"cell";
-//    static NSString *adCellID = @"adCell";
-//    NSDictionary  *info =  [self.dataArray objectAtIndex:indexPath.row];
-//    if ([info isKindOfClass:[NSString class]]) {
-//        AdvertisementCell *cell = [self.mytableView dequeueReusableCellWithIdentifier:adCellID];
-//        if (cell == nil) {
-//            cell = [[AdvertisementCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:adCellID];
-//        }
-//        return cell;
-//    }else{
-//        InfoCell *cell = [self.mytableView dequeueReusableCellWithIdentifier:cellID];
-//        if (cell == nil) {
-//            cell = [[InfoCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
-//        }
-//        [cell setInfo:info];
-//        headimage = [[UIImageView alloc] initWithFrame:CGRectMake(20, 5, 30, 30)];
-//        NSString *imageName = [NSString stringWithFormat:@"%d.jpg",indexPath.row%50+1];
-//        [headimage setImage:[UIImage imageNamed:imageName]];
-//        [headimage setTag:10002];
-//        [cell addSubview:headimage];
-//        return cell;
-//    }
-//    return nil;
-//}
 
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
